3fb662ec07d79388ae3afe8b205beeaba56366c7
Mung <munglore@gmail.com>
2013-06-21 20:49:40 -0400

    Initial commit of test project

---
+++ KinectTherapyTest/Criteria/JointAnalyzer.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using Microsoft.Xna.Framework;
+using NUnit.Framework;
+using System.Diagnostics;
+
+namespace SWENG.Criteria
+{
+    /// <summary>
+    /// The purpose of the joint analyzer is to encapsulate the mathematics behind comparing joints.
+    /// These methods should return a numerical result representing the degree off perfect alignment.
+    /// 
+    /// </summary>
+    public class JointAnalyzer
+    {
+        /// <summary>
+        /// to see if the joints are aligned we must choose one joint as the starting point (NOT THE CENTER) and compare to the other two joints
+        /// the Dot product will determine if they are in alignment. 
+        /// 1 = parallel; 0 = perpendicular; -1 = pointing opposite directions
+        /// </summary>
+        /// <param name="centerJoint"></param>
+        /// <param name="otherJoints"></param>
+        /// <returns></returns>
+        public static float areJointsAligned(Joint centerJoint, Joint[] otherJoints)
+        {
+            // 
+            Joint startingJoint = otherJoints[0];
+            Joint endingJoint = otherJoints[1];
+
+            Vector3 vector0 = new Vector3(centerJoint.Position.X - startingJoint.Position.X, centerJoint.Position.Y - startingJoint.Position.Y, centerJoint.Position.Z - startingJoint.Position.Z);
+            // get vector vertex -> joint1
+            Vector3 vector1 = new Vector3(endingJoint.Position.X - startingJoint.Position.X, endingJoint.Position.Y - startingJoint.Position.Y, endingJoint.Position.Z - startingJoint.Position.Z);
+
+            // Normalize vectors so we can use Dot product correctly
+            vector0.Normalize();
+            vector1.Normalize();
+            return Vector3.Dot(vector0, vector1);
+        }
+
+        /// <summary>
+        /// Negative response means aligning joint is below joint to be aligned to
+        /// Positive response means aligning joint is above joint to be aligned to. 
+        /// 0 means they are at the same level
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static float alignedHorizontally(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            // KISS then expand
+            return aligningJoint.Position.Y - jointToBeAlignedTo.Position.Y;
+        }
+
+        /// <summary>
+        /// Negative response means aligning joint is to the left of joint to be aligned to
+        /// Positive response means aligning joint is to the right of joint to be aligned to. 
+        /// 0 means they are at the same level
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static float alignedVertically(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            return aligningJoint.Position.X - jointToBeAlignedTo.Position.X;
+        }
+
+        /// <summary>
+        /// Determines if the aligningJoint is to the left of the jointToBeAlignedTo
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static bool isLeftOf(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            return alignedVertically(aligningJoint, jointToBeAlignedTo) < 0;
+        }
+
+        /// <summary>
+        /// Determines if the aligningJoint is to the right of the jointToBeAlignedTo
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static bool isRightOf(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            return alignedVertically(aligningJoint, jointToBeAlignedTo) > 0;
+        }
+
+        /// <summary>
+        /// Determines if the aligningJoint is above the jointToBeAlignedTo
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static bool isAbove(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            return alignedHorizontally(aligningJoint, jointToBeAlignedTo) > 0;
+        }
+
+        /// <summary>
+        /// Determines if the aligningJoint is below the jointToBeAlignedTo
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static bool isBelow(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            return alignedHorizontally(aligningJoint, jointToBeAlignedTo) < 0;
+        }
+
+        /// <summary>
+        /// Determines if the aligningJoint is in front of the jointToBeAlignedTo
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static bool isAhead(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            return alignedHorizontally(aligningJoint, jointToBeAlignedTo) < 0;
+        }
+
+        /// <summary>
+        /// Determines if the aligningJoint is behind the jointToBeAlignedTo
+        /// </summary>
+        /// <param name="aligningJoint"></param>
+        /// <param name="jointToBeAlignedTo"></param>
+        /// <returns></returns>
+        public static bool isBehind(Joint aligningJoint, Joint jointToBeAlignedTo)
+        {
+            return alignedHorizontally(aligningJoint, jointToBeAlignedTo) < 0;
+        }
+
+        /// <summary>
+        /// Determines the angle between three joints
+        /// </summary>
+        /// <param name="vertexJoint"></param>
+        /// <param name="adjacentJoints"></param>
+        /// <returns></returns>
+        public static int findAngle(Joint vertexJoint, Joint[] adjacentJoints)
+        {
+            // get the vertex and other joints off the skeleton stamp
+            Joint joint0 = adjacentJoints[0];
+            Joint joint1 = adjacentJoints[1];
+            // get vector vertex -> joint0
+
+            Vector3 vector0 = new Vector3(joint0.Position.X - vertexJoint.Position.X, joint0.Position.Y - vertexJoint.Position.Y, joint0.Position.Z - vertexJoint.Position.Z);
+            // get vector vertex -> joint1
+            Vector3 vector1 = new Vector3(joint1.Position.X - vertexJoint.Position.X, joint1.Position.Y - vertexJoint.Position.Y, joint1.Position.Z - vertexJoint.Position.Z);
+
+            //// determine the angle from two vectors in a 2D space...
+            // the old way
+            //float computedAngle = (float)Math.Atan2(vector0.Y, vector0.X) - (float)Math.Atan2(vector1.Y, vector1.X);
+            //// compare to the varience
+            //int convertedAngle = Convert.ToInt32(computedAngle * (180.0 / Math.PI));
+
+            // the right way we're in 3D
+            // using vector provided methods
+            vector0.Normalize();
+            vector1.Normalize();
+            float dotAngle = (float)Math.Acos(Vector3.Dot(vector0, vector1));
+            int convertedDotAngle = Convert.ToInt32(dotAngle * (180.0 / Math.PI));
+            return convertedDotAngle;
+        }
+    }
+}
---
+++ KinectTherapyTest/Test/JointAnalyzerTest.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Microsoft.Kinect;
+
+namespace SWENG.Criteria
+{
+    [TestFixture]
+    public class JointAnalyzerTest
+    {
+
+        [SetUp]
+        public void Init()
+        {
+        }
+
+        [TestAttribute]
+        public void TestJointsAligned()
+        {
+            #region align on the x and z axis
+            Joint center = new Joint();
+            Joint[] otherJoints = new Joint[2];
+
+            SkeletonPoint sp = new SkeletonPoint();
+            sp.X = 1.0F;
+            sp.Y = 1.0F;
+            sp.Z = 1.0F;
+            center.Position = sp;
+
+            Joint joint1 = new Joint();
+            SkeletonPoint point = new SkeletonPoint();
+            point.X = 1.0F;
+            point.Y = 0.0F;
+            point.Z = 1.0F;
+            joint1.Position = point;
+
+            Joint joint2 = new Joint();
+            SkeletonPoint point2 = new SkeletonPoint();
+            point2.X = 1.0F;
+            point2.Y = 2.0F;
+            point2.Z = 1.0F;
+            joint2.Position = point2;
+
+            otherJoints[0] = joint1;
+            otherJoints[1] = joint2;
+
+            Assert.AreEqual(1.0F, JointAnalyzer.areJointsAligned(center, otherJoints), "Joints should be aligned but are not: X and Z axis");
+            #endregion
+
+            #region align on the y and z axis
+            center = new Joint();
+            otherJoints = new Joint[2];
+
+            sp = new SkeletonPoint();
+            sp.X = 1.0F;
+            sp.Y = 1.0F;
+            sp.Z = 1.0F;
+            center.Position = sp;
+
+            joint1 = new Joint();
+            point = new SkeletonPoint();
+            point.X = 21.0F;
+            point.Y = 1.0F;
+            point.Z = 1.0F;
+            joint1.Position = point;
+
+            joint2 = new Joint();
+            point2 = new SkeletonPoint();
+            point2.X = 0.0F;
+            point2.Y = 1.0F;
+            point2.Z = 1.0F;
+            joint2.Position = point2;
+
+            otherJoints[0] = joint1;
+            otherJoints[1] = joint2;
+
+            Assert.AreEqual(1.0F, JointAnalyzer.areJointsAligned(center, otherJoints), "Joints should be aligned but are not: Y and Z axis");
+            #endregion
+
+            #region align on the x and y axis
+            center = new Joint();
+            otherJoints = new Joint[2];
+
+            sp = new SkeletonPoint();
+            sp.X = 1.0F;
+            sp.Y = 1.0F;
+            sp.Z = 1.0F;
+            center.Position = sp;
+
+            joint1 = new Joint();
+            point = new SkeletonPoint();
+            point.X = 1.0F;
+            point.Y = 1.0F;
+            point.Z = 0.0F;
+            joint1.Position = point;
+
+            joint2 = new Joint();
+            point2 = new SkeletonPoint();
+            point2.X = 1.0F;
+            point2.Y = 1.0F;
+            point2.Z = 2.0F;
+            joint2.Position = point2;
+
+            otherJoints[0] = joint1;
+            otherJoints[1] = joint2;
+
+            Assert.AreEqual(1.0F, JointAnalyzer.areJointsAligned(center, otherJoints), "Joints should be aligned but are not: X and Y axis");
+            #endregion
+        }
+
+        [TestAttribute]
+        public void TestJointsMisaligned()
+        {
+            #region perpendicular
+            Joint center = new Joint();
+            Joint[] otherJoints = new Joint[2];
+
+            SkeletonPoint sp = new SkeletonPoint();
+            sp.X = 0.0F;
+            sp.Y = 1.0F;
+            sp.Z = 0.0F;
+            center.Position = sp;
+
+            Joint joint1 = new Joint();
+            SkeletonPoint point = new SkeletonPoint();
+            point.X = 0.0F;
+            point.Y = 0.0F;
+            point.Z = 0.0F;
+            joint1.Position = point;
+
+            Joint joint2 = new Joint();
+            SkeletonPoint point2 = new SkeletonPoint();
+            point2.X = 1.0F;
+            point2.Y = 0.0F;
+            point2.Z = 0.0F;
+            joint2.Position = point2;
+
+            otherJoints[0] = joint1;
+            otherJoints[1] = joint2;
+
+            Assert.AreEqual(0.0F, JointAnalyzer.areJointsAligned(center, otherJoints), "Joint vectors should be orthogonal (90 degrees)");
+            #endregion
+        }
+    }
+}
