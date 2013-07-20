09b5cc64a04cc0f192c7d035db49b764ed7ef4bf
Mung <munglore@gmail.com>
2013-07-19 22:32:54 -0400

    forgot to check in a class

--- KinectTherapyTest/Criteria/AngleCriterion.cs
+++ KinectTherapyTest/Criteria/AngleCriterion.cs
@@ -127,5 +127,18 @@ namespace SWENG.Criteria
             results[(int)adjacentJoints[1].JointType] = normalizedAccuracy;
             return results;
         }
+
+        public override List<Joint> MatchSkeletonToCriterion()
+        {
+            List<JointType> jointTypes = new List<JointType>();
+
+            jointTypes.Add(Vertex.GetJointType());
+            foreach (XmlJointType xmlJointType in OtherJoints)
+            {
+                jointTypes.Add(xmlJointType.GetJointType());
+            }
+            return null;
+        }
+
     }
 }
